6a907bcb2cc4014916b5cf4f87a4f184f128b80d
Vamsi Devalla <vammichanti40312@gmail.com>
2018-05-08 11:42:41 -0400

    made the search component to respond to change in number of results

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -26,7 +26,11 @@ class Search extends Component{
         }
     }
 
-    onAmountChange=(e,index,value)=>this.setState({amount:value})
+    onAmountChange=(e,index,value)=>this.setState({amount:value},()=>{
+        axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
+            .then(res=>this.setState({images:res.data.hits}))
+            .catch(err => console.log(err))
+    })
 
     render(){
         console.log(this.state)
